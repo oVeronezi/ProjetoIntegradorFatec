8c993a0bdf0d1ca234739ea9ee0a32910f535169
Luis Felipe Veronezi Bernardo <luisfelipevbenardo@gmail.com>
2025-10-21 10:20:09 -0300

    alterações na classe entrega ajuste de tipo de id

--- ControleDietaHospitalarUnimedJau/Models/Entrega.cs
+++ ControleDietaHospitalarUnimedJau/Models/Entrega.cs
@@ -6,16 +6,16 @@ namespace ControleDietaHospitalarUnimedJau.Models
 {
     public class Entrega
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public DateTime HoraInicio { get; set; }
         public DateTime? HoraFim { get; set; }  // ADICIONAR ? AQUI
         public string Status { get; set; }
         public string Observacao { get; set; }
-        public int PacienteId { get; set; }
+        public Guid PacienteId { get; set; }
         public Paciente Paciente { get; set; }
-        public int CopeiraId { get; set; }
+        public Guid CopeiraId { get; set; }
         public Copeira Copeira { get; set; }
-        public int? DietaId { get; set; }
+        public Guid? DietaId { get; set; }
         public Dieta Dieta { get; set; }
 
         public TimeSpan? CalcularTempoEntrega()  // TAMBÉM TORNAR nullable
